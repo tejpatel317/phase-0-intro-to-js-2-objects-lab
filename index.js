4cad3aebb139d5de9485f240e6f13a1a639b8806
Tej Patel <tejpatel317@gmail.com>
2022-04-28 21:10:33 -0400

    commit tree d7371a1cadf8800c0bf65568769939b9b08252fc

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Tej',
+    streetAdress: '380 River Drive',
+}
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    return {...employeeObject,[key] : value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    employeeObject[key] = value;
+    return employeeObject;
+}
+
+function deleteFromEmployeeByKey(employeeObject, key) {
+    const copyOfEmployee = {...employeeObject};
+    delete copyOfEmployee[key];
+    return copyOfEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+    delete employeeObject[key];
+    return employeeObject;
+}
